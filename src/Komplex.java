56e015eea2f9af654a6fbcf9fd031f99461091fd
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2022-03-22 15:11:28 +0100

    Arbetat

---
+++ src/Komplex.java
@@ -0,0 +1,20 @@
+public class Komplex {
+    double a;
+    double b;
+
+    public Komplex(double v, double v1) {
+    }
+
+
+    public Komplex add(Komplex b) {
+        return  b;
+    }
+
+    public Komplex multiply(Komplex b) {
+        return b;
+    }
+
+    public Double arg() {
+        return a;
+    }
+}
